6ac2baa3ce256516a9187e7b1cc0d5cb0e359629
ADSharnin <adsharnin@gmail.com>
2023-08-08 02:20:14 +0300

    add map, contacts done

--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
+
+import photo from '../../images/photo.jpg';
 
 import styles from './Contacts.module.scss'
 
 const Contacts = (): JSX.Element => {
     window.scrollTo(0, 0);
+
+    const defaultState = {
+        center: [59.98701999279996,30.317567368884085],
+        zoom: 16,
+    };
 
     return (
         <>
@@ -32,10 +40,33 @@ const Contacts = (): JSX.Element => {
                             </div>
                         </div>
                         <div className={styles.MapBlock}>
-                            <div className={styles.MapBorder}></div>
+                            <div className={styles.MapBorder}>
+                                <YMaps>
+                                    <Map defaultState={defaultState} className={styles.Map}>
+                                        <Placemark geometry={[59.98701999279996,30.317567368884085]}/>
+                                    </Map>
+                                </YMaps>
+                            </div>
                         </div>
                     </div>
-                    <div className={styles.BottomBlock}></div>
+                    <div className={styles.BottomBlock}>
+                        <div className={styles.ContactImageBorder}>
+                            <img
+                                className={styles.ContactImage}
+                                src={photo}
+                            />
+                        </div>
+                        <div className={styles.ContactText}>
+                            <p>Компания «Квантовая оптика» создана в 2011 году с целью выполнения наукоемких проектов по разработке, производству и внедрению лазерных приборов и систем.
+                            Приоритетным направлением деятельности компании являются научные исследования и разработки, направленные на создание твердотельных лазеров нового поколения с диодной накачкой и современных оптико-электронных систем для жестких условий эксплуатации.</p>
+                            <br/>
+                            <p>Высокая квалификация сотрудников компании позволяет в сжатые сроки выполнять полный цикл работ от НИР и ОКР до производства и адаптации оборудования у конкретного потребителя, сократить стоимость создания нового продукта.
+                            Компания использует современную элементную базу, новые материалы и технологии.</p>
+                            <br/>
+                            <p>Постоянно ведется поиск новых принципов построения лазерных систем, совершенствуются конструкторские решения, внедряются новые технологические процессы.
+                            Заказчику предлагается техническое сопровождение на этапе освоения новой лазерной техники и сервисное обслуживание действующего оборудования.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className={styles.Copy}>ООО «Квантовая оптика» Copyright © 2023</div>
